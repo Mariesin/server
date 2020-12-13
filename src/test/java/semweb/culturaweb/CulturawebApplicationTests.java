16abbed4b5ef16b7158a91efc64d5c038a7fd8f0
Marie <marie.sinama@hotmail.fr>
2020-12-13 10:55:21 +0100

    ui semweb

---
+++ src/test/java/semweb/culturaweb/CulturawebApplicationTests.java
@@ -0,0 +1,13 @@
+package semweb.culturaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulturawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
