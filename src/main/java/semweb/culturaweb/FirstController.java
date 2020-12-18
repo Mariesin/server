167d59fbb0a9ec435ec59328060f1e2b6fd5af7d
Mariesin <32493950+Mariesin@users.noreply.github.com>
2020-12-18 20:37:25 +0100

    Update FirstController.java

--- src/main/java/semweb/culturaweb/FirstController.java
+++ src/main/java/semweb/culturaweb/FirstController.java
@@ -1,20 +1,29 @@
 package semweb.culturaweb;
 
-import org.apache.jena.rdf.model.Literal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import java.util.List;
 
 @Controller
 public class FirstController {
     CreateModel m = new CreateModel();
-    List<Literal> list = m.findName();
+    List<SalleSpectacle> listSalleSpectacle = m.findAllSalleSpectacle();
+    List<Cinema> listCinema = m.findAllCinema();
 
     @RequestMapping(value = "/liste-cultura")
-    public String getAll(Model model) {
-        model.addAttribute("salle", list);
+    public String getAllSalleSpectacle(Model model) {
+        System.out.println(listSalleSpectacle);
+        model.addAttribute("salleSpectacle", listSalleSpectacle);
         return "liste-cultura";
+    }
+
+    @RequestMapping(value = "/liste-cinema")
+    public String getAllCinema(Model model) {
+        System.out.println(listCinema);
+        model.addAttribute("salleCinema", listCinema);
+        return "liste-cinema";
     }
 }
